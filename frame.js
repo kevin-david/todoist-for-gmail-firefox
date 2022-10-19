45670a1fc5ec7ba2260f7ce4a99665b29c2a6d73
Kevin David <kevin-david@github.com>
2022-10-19 18:12:39 -0400

    Fixing some console errors

--- frame.js
+++ frame.js
@@ -2,8 +2,8 @@ function init() {
     TODOIST_FRAME = document.getElementById('todoist_frame');
 
     window.addEventListener('message', function(e) {
-        if(e.data.indexOf('--/--') != -1 || e.data.indexOf('GMAIL_MESSAGE_ADD') != -1)
-            TODOIST_FRAME.contentWindow.postMessage(e.data, '*');
+        if(typeof e.data === 'string' && (e.data.indexOf('--/--') != -1 || e.data.indexOf('GMAIL_MESSAGE_ADD') != -1))
+        TODOIST_FRAME.contentWindow.postMessage(e.data, '*');
     });
 }
 
